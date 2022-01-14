d399cdb4fb69d13a2e60946199ca2ec0d8ee599c
joedomabylv <joedomabylv@gmail.com>
2022-01-14 10:26:43 -0700

    basic framework

---
+++ project_1/Client.java
@@ -0,0 +1,15 @@
+/* TODO:
+   1. JOIN a chat
+   2. LEAVE a chat (without terminating itself)
+   3. SHUTDOWN itself, which terminates (first leave chat, then terminate)
+   4. SHUTDOWNALL, which shutsdown itself, all other clients, and the server
+   5. NOTES (regular messages)
+ */
+
+public class Client {
+    
+    public static void main(String[] args) {
+	System.out.println("I am the client!");
+    }
+    
+}
---
+++ project_1/Server.java
@@ -0,0 +1,10 @@
+/* TODO:
+   1. Register for participation in chat (JOIN)
+   2. Leave the chat
+   3. Send messages to all participating clients
+ */
+public class Server {
+    public static void main(String[] args) {
+	System.out.println("I am the server!");
+    }
+}
